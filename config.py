03b998bcc71aeb699449a121fd06e9ee65b484e4
be1shebaevvv <ermekalymbaev2805@gmail.com>
2023-03-15 19:47:41 +0600

    init_commit

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "5886515137:AAGze8jLm6y-OrKHWcRB2cMa1zm-tguXQp0"
+
+
+DATABASE_USER = "root"
+DATABASE_PASSWORD = "qwerty123"
+DATABASE_HOST = "localhost"
+DATABASE_NAME = "car_db"
+
+# Ссылка на базу данных
+MYSQL_URL = f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+
+
+
